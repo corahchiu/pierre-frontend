b22a62ef11ee209a4de8c5b27e21591930ad3562
Corah Chiu <corahchiu@gmail.com>
2018-05-19 21:54:19 +0800

    initial commit

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,31 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  list = ["bonjour", "monsieur", "merci", "merci beaucoup"];
+
+  constructor(public navCtrl: NavController, private tts: TextToSpeech) {
 
   }
 
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  speak(word, language) {
+    return this.tts.speak({
+      text: word,
+      locale: language
+    })
+    .then(() => console.log('Success'))
+    .catch((reason: any) => console.log(reason));
+  }
+  
+  
+  async readList(list) {
+    list = this.list;
+    for (let i=0; i<list.length; i++) {
+      await this.speak(list[i], "fr-FR")
+      await this.sleep(1000);
+    }
+  }
 }
